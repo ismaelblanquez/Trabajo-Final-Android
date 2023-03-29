94467b9571885db615048d45f98682d23d1259a5
ismaelblanquez <ismaelblanquez@gmail.com>
2023-03-30 00:40:04 +0200

    30/03/23 0:39
    Terminado

--- app/src/main/java/com/example/ibooking/lstHotels/view/HomeActivity.java
+++ app/src/main/java/com/example/ibooking/lstHotels/view/HomeActivity.java
@@ -4,18 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ibooking.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
     Button btnSeeAll,btnChoose,btnRating, btnHistory;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ImageButton btn_logout;
+        btn_logout = findViewById(R.id.logout_button);
+        mAuth = FirebaseAuth.getInstance();
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAuth.signOut();
+                finish();
+                startActivity(new Intent(HomeActivity.this, LoginActivity.class));
+            }
+        });
+
 
         btnSeeAll = (Button) findViewById(R.id.btn_seeAll);
 
